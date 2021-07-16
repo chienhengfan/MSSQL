95c6ad46e29b493aeb8e0b8071b67533a3fa0c89
chienheng fan <72858026+chienhengfan@users.noreply.github.com>
2021-07-16 08:34:33 +0800

    Update query_Mssql.py

--- query_Mssql.py
+++ query_Mssql.py
@@ -20,21 +20,22 @@ class MSSQL:
         conn.close()
         return df
 
-    #insert:codition
-    #output: dataframe of check data inserted
-    # def insert_data(host,user,pw,DB,columns,target_table,source_table,tuple_CUS_NO):
-    #     conn = pyodbc.connect('DRIVER=ODBC Driver 17 for SQL Server;SERVER={0};DATABASE={3};UID={1};PWD={2}'.format(
-    #         host, user, pw, DB
-    #     ))
-    #
-    #     cursor = conn.cursor()
-    #
-    #     cursor.execute("INSERT INTO {0}({1}) SELECT {1} FROM {2} WHERE CUS_NO IN {3}".format(target_table,columns,source_table,tuple_CUS_NO))
-    #     df_check = pd.read_sql("SELECT * FROM {0} WHERE CUS_NO IN {1}".format(target_table,tuple_CUS_NO),conn)
-    #
-    #     cursor.close()
-    #     conn.close()
-    #     return df_check
+    insert:codition
+    output: dataframe of check data inserted
+    def insert_data(host,user,pw,DB,columns,target_table,source_table,tuple_CUS_NO):
+        conn = pyodbc.connect('DRIVER=ODBC Driver 17 for SQL Server;SERVER={0};DATABASE={3};UID={1};PWD={2}'.format(
+            host, user, pw, DB
+        ))
+    
+        cursor = conn.cursor()
+    
+        cursor.execute("INSERT INTO {0}({1}) SELECT {1} FROM {2} WHERE CUS_NO IN {3}".format(target_table,columns,source_table,tuple_CUS_NO))
+         conn.commit()
+        df_check = pd.read_sql("SELECT * FROM {0} WHERE CUS_NO IN {1}".format(target_table,tuple_CUS_NO),conn)
+          
+        cursor.close()
+        conn.close()
+        return df_check
 
 
 
@@ -51,11 +52,11 @@ if __name__ == '__main__':
     user = read_json('D:/secret/sunlike_DB/','user.txt')
     pw = read_json('D:/secret/sunlike_DB/','password.txt')
 
-    DB = 'DB_WH01'
+    DB = '{your DB}'
     query1 = "SELECT * FROM {your table}"
 
     df_cust_wh01 = MSSQL.get_data(host,user,pw,DB,query1)
-    df_cust_wh01.to_csv('./MF_BOM_WH01.csv',encoding='utf_8_sig')
+    df_cust_wh01.to_csv('./{your export file name}.csv',encoding='utf_8_sig')
 
 
     #
